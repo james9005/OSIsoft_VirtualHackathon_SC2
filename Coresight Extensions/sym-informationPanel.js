107334c23a956ffa6a16ed97de8f4b93f77815e9
James Todd <james9005@hotmail.co.uk>
2017-03-13 21:46:22 +0000

    tidied up repository

--- Coresight Extensions/sym-informationPanel.js
+++ Coresight Extensions/sym-informationPanel.js
@@ -8,8 +8,23 @@
 
         function dataUpdate(data) {
             if (data) {
-                console.log(data);
+                // console.log(data);
+                if(data.Data.length != undefined){
+                    // this loads the required fields for the informationPanel & checks that the data is not undefined.
 
+
+                    //write the datafields to the table
+                    $('#callsLogged').text(data.Data[0].Value);
+                    $('#slaMet').text(data.Data[1].Value);
+                    $('#incidents').text(data.Data[2].Value);
+                    $('#requests').text(data.Data[3].Value);
+                    $('#callsResolved').text(data.Data[4].Value);
+                    $('#failedSLA').text(data.Data[5].Value);
+                    $('#bestPerformer').text(data.Data[6].Value);
+                    $('#worstPerformer').text(data.Data[7].Value);
+
+
+                }
 
 
 
@@ -40,33 +55,3 @@
 
     CS.symbolCatalog.register(definition);
 })(window.PIVisualization);
-
-
-
-
-
-// Object
-// Data
-// :
-// Array[31]
-// 0
-// :
-// Object
-// EndTime
-// :
-// "3/11/2017 2:01:09 PM"
-// ErrorCode
-// :
-// 3503
-// ErrorDescription
-// :
-// "PI Point Not Found \\PISRV01\Daily Rollup1.Actual EOD"
-// StartTime
-// :
-// "3/11/2017 6:01:09 AM"
-// Values
-// :
-// Array[0]
-// __proto__
-// :
-// Object
